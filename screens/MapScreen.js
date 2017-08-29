1d2adca6e2e58a9b0463e3851338489a395255c6
dnp <doanhpham1995@gmail.com>
2017-08-28 22:23:56 -0400

    finished 113

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, ActivityIndicator } from 'react-native';
+import { MapView } from 'expo';
 
 class MapScreen extends Component {
+  state = {
+    mapLoaded: false,
+    region: {
+      longitude: -122,
+      latitude: 37,
+      longitudeDelta: 0.04,
+      latitudeDelta: 0.09
+    }
+  };
+
+  componentDidMount() {
+    this.setState({ mapLoaded: true });
+  }
+
+  onRegionChangeComplete = (region) => {
+    this.setState({ region });
+  }
+
   render() {
+    if (!this.state.mapLoaded) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <ActivityIndicator size="large" />
+        </View>
+      );
+    }
+
     return (
       <View style={{ flex: 1 }}>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
+        <MapView
+          region={this.state.region}
+          style={{ flex: 1 }}
+          onRegionChangeComplete={this.onRegionChangeComplete}
+        />
       </View>
     );
   }
