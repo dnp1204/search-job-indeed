40b9108c75cc206fdd1afba64a95c53b78280235
dnp <doanhpham1995@gmail.com>
2017-08-29 22:04:44 -0400

    finished 129

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -1,19 +1,68 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform } from 'react-native';
+import { connect } from 'react-redux';
+import { MapView } from 'expo';
+import { Card, Button } from 'react-native-elements';
+import * as actions from '../actions';
+import Swipe from '../components/Swipe';
 
 class DeckScreen extends Component {
+  renderCard(job) {
+    const initialRegion = {
+      longitude: job.longitude,
+      latitude: job.latitude,
+      longitudeDelta: 0.02,
+      latitudeDelta: 0.045
+    };
+
+    return (
+      <Card title={job.jobtitle}>
+        <View style={{ height: 300 }}>
+          <MapView
+            scrollEnabled={false}
+            style={{ flex: 1 }}
+            cacheEnabled={Platform.OS === 'android' ? true : false}
+            initialRegion={initialRegion}
+          />
+        </View>
+        <View style={styles.detailWrapper}>
+          <Text>{job.company}</Text>
+          <Text>{job.formattedRelativeTime}</Text>
+        </View>
+        <Text style={{ height: 70 }}>{job.snippet.replace(/<b>/g, '').replace(/<\/b>/g, '')}</Text>
+      </Card>
+    );
+  }
+
+  renderNoMoreCards() {
+    return <Card title="No more card" />;
+  }
+
   render() {
     return (
-      <View style={{ flex: 1 }}>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
+      <View style={{ marginTop: 10 }}>
+        <Swipe
+          data={this.props.jobs}
+          renderCard={this.renderCard}
+          renderNoMoreCards={this.renderNoMoreCards}
+          onSwipeRight={job => this.props.likeJob(job)}
+          keyProp='jobkey'
+        />
       </View>
     );
   }
 }
 
-export default DeckScreen;
+const styles = {
+  detailWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: 10
+  }
+};
+
+function mapStateToProps(state) {
+  return { jobs: state.jobs.results };
+}
+
+export default connect(mapStateToProps, actions)(DeckScreen);
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { View, ActivityIndicator } from 'react-native';
 import { MapView } from 'expo';
+import { Button } from 'react-native-elements';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class MapScreen extends Component {
   state = {
@@ -17,9 +20,15 @@ class MapScreen extends Component {
     this.setState({ mapLoaded: true });
   }
 
-  onRegionChangeComplete = (region) => {
+  onRegionChangeComplete = region => {
     this.setState({ region });
-  }
+  };
+
+  onButtonPress = () => {
+    this.props.fetchJobs(this.state.region, () => {
+      this.props.navigation.navigate('deck');
+    });
+  };
 
   render() {
     if (!this.state.mapLoaded) {
@@ -37,9 +46,27 @@ class MapScreen extends Component {
           style={{ flex: 1 }}
           onRegionChangeComplete={this.onRegionChangeComplete}
         />
+        <View style={styles.buttonContainer}>
+          <Button
+            large
+            title="Search This Area"
+            backgroundColor="#009688"
+            icon={{ name: 'search' }}
+            onPress={this.onButtonPress}
+          />
+        </View>
       </View>
     );
   }
 }
 
-export default MapScreen;
+const styles = {
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 20,
+    left: 0,
+    right: 0
+  }
+};
+
+export default connect(null, actions)(MapScreen);
